684adf2a07c213dcc4ed112265acfec4aceea513
maciekiwaniuk <iwaniukmaciej.kontakt@gmail.com>
2023-09-06 21:31:21 +0200

    Update User entity

--- src/Domain/Entity/User.php
+++ src/Domain/Entity/User.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\Entity;
 
 use App\Infrastructure\Doctrine\Repository\UserRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
@@ -14,5 +15,177 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email.')]
 class User
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
+    #[ORM\Column(length: 200, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 100, unique: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 100, unique: true)]
+    private ?string $surname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $lastLoginIp = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $lastLoginTime = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $lastLoginUserAgent = null;
+
+    #[ORM\Column]
+    private DateTimeImmutable $updatedAt;
+
+    #[ORM\Column]
+    private DateTimeImmutable $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+        $this->updatedAt = new DateTimeImmutable();
+    }
+
+    #[ORM\PreUpdate]
+    public function setUpdatedAtValue(): void
+    {
+        $this->updatedAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function isAdmin(): bool
+    {
+        if (in_array('ROLE_ADMIN', $this->roles)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getLastLoginIp(): ?string
+    {
+        return $this->lastLoginIp;
+    }
+
+    public function setLastLoginIp(?string $lastLoginIp): self
+    {
+        $this->lastLoginIp = $lastLoginIp;
+
+        return $this;
+    }
+
+    public function getLastLoginTime(): ?string
+    {
+        return $this->lastLoginTime;
+    }
+
+    public function setLastLoginTime(string $lastLoginTime): self
+    {
+        $this->lastLoginTime = $lastLoginTime;
+
+        return $this;
+    }
+
+    public function getLastLoginUserAgent(): ?string
+    {
+        return $this->lastLoginUserAgent;
+    }
+
+    public function setLastLoginUserAgent(string $lastLoginUserAgent): self
+    {
+        $this->lastLoginUserAgent = $lastLoginUserAgent;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): ?DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
 }
