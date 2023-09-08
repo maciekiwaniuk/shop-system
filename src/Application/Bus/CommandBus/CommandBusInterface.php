b216ee909e786e0f43c845b8d30b44f0cdb286e9
maciekiwaniuk <iwaniukmaciej.kontakt@gmail.com>
2023-09-08 21:57:00 +0200

    Add missing strict types

--- src/Application/Bus/CommandBus/CommandBusInterface.php
+++ src/Application/Bus/CommandBus/CommandBusInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Application\Bus\CommandBus;
 
 use App\Application\BusResult\CommandResult;
--- src/Application/Bus/QueryBus/QueryBus.php
+++ src/Application/Bus/QueryBus/QueryBus.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Application\Bus\QueryBus;
 
 use App\Application\BusResult\CommandResult;
