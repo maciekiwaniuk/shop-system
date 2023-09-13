1c30c27d3b32f47dd5fafb06244b5908d345f261
maciekiwaniuk <iwaniukmaciej.kontakt@gmail.com>
2023-09-13 10:39:58 +0200

    Update JsonSerializer

--- src/Infrastructure/Serializer/JsonSerializer.php
+++ src/Infrastructure/Serializer/JsonSerializer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\Serializer;
 
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -15,8 +16,15 @@ class JsonSerializer
 
     public function __construct()
     {
+        $dateTimeNormalizer = new DateTimeNormalizer();
+        $dateTimeNormalizer
+            ->setDefaultContext([
+                DateTimeNormalizer::FORMAT_KEY => 'Y-m-d H:i:s',
+                DateTimeNormalizer::TIMEZONE_KEY => null
+            ]);
+
         $this->serializer = new Serializer(
-            [new ObjectNormalizer()],
+            [$dateTimeNormalizer, new ObjectNormalizer()],
             [new JsonEncoder()]
         );
     }
