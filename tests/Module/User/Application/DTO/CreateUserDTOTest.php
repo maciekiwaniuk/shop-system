be4f6524352b831832b0783c200c5c038c0c0e1f
macieeek <iwaniukmaciej.kontakt@gmail.com>
2023-10-19 14:36:37 +0200

    Update CreateUserDTOTest

--- tests/Module/User/Application/DTO/CreateUserDTOTest.php
+++ tests/Module/User/Application/DTO/CreateUserDTOTest.php
@@ -11,6 +11,10 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class CreateUserDTOTest extends AbstractUnitTestCase
 {
     protected object $validator;
+    protected string $exampleValidEmail = 'example@email.com';
+    protected string $exampleValidPassword = 'example123';
+    protected string $exampleValidName = 'John';
+    protected string $exampleValidSurname = 'Muller';
 
     protected function setUp(): void
     {
@@ -21,14 +25,98 @@ class CreateUserDTOTest extends AbstractUnitTestCase
     public function testValidData(): void
     {
         $dto = new CreateUserDTO(
-            email: 'example@email.com',
-            password: 'example123',
-            name: 'John',
-            surname: 'Muller'
+            email: $this->exampleValidEmail,
+            password: $this->exampleValidPassword,
+            name: $this->exampleValidName,
+            surname: $this->exampleValidSurname
         );
 
         $errors = $this->validator->validate($dto);
 
         $this->assertEmpty($errors);
+    }
+
+    public function invalidEmailProvider(): iterable
+    {
+        yield [''];
+        yield ['mailWithoutAt'];
+        yield [str_repeat('o', 101) . '@example.com'];
+    }
+
+    public function testInvalidEmail(string $email): void
+    {
+        $dto = new CreateUserDTO(
+            email: $email,
+            password: $this->exampleValidPassword,
+            name: $this->exampleValidName,
+            surname: $this->exampleValidSurname
+        );
+
+        $errors = $this->validator->validate($dto);
+
+        $this->assertCount(1, $errors);
+    }
+
+    public function invalidPasswordProvider(): iterable
+    {
+        yield [''];
+        yield ['1'];
+        yield [str_repeat('o', 101)];
+    }
+
+    public function testInvalidPassword(string $password): void
+    {
+        $dto = new CreateUserDTO(
+            email: $this->exampleValidEmail,
+            password: $password,
+            name: $this->exampleValidName,
+            surname: $this->exampleValidSurname
+        );
+
+        $errors = $this->validator->validate($dto);
+
+        $this->assertCount(1, $errors);
+    }
+
+    public function invalidNameProvider(): iterable
+    {
+        yield [''];
+        yield ['1'];
+        yield [str_repeat('o', 101)];
+    }
+
+    public function testInvalidName(string $name): void
+    {
+        $dto = new CreateUserDTO(
+            email: $this->exampleValidEmail,
+            password: $this->exampleValidPassword,
+            name: $name,
+            surname: $this->exampleValidSurname
+        );
+
+        $errors = $this->validator->validate($dto);
+
+        $this->assertCount(1, $errors);
+    }
+
+    public function invalidSurnameProvider(): iterable
+    {
+        yield [''];
+        yield ['1'];
+        yield [str_repeat('o', 101)];
+    }
+
+    public function testInvalidSurname(string $surname): void
+    {
+        $dto = new CreateUserDTO(
+            email: $this->exampleValidEmail,
+            password: $this->exampleValidPassword,
+            name: $this->exampleValidName,
+            surname: $surname
+        );
+
+        $errors = $this->validator->validate($dto);
+
+        $this->assertCount(1, $errors);
     }
 }
