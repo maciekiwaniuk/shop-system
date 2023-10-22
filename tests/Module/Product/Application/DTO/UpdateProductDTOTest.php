dbb1e4e53c83b5a2c436d9a7968e051000ba348f
maciekiwaniuk <iwaniukmaciej.kontakt@gmail.com>
2023-10-22 08:24:29 +0200

    Add UpdateProductDTOTest

---
+++ tests/Module/Product/Application/DTO/UpdateProductDTOTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Module\Product\Application\DTO;
+
+use App\Module\Product\Application\DTO\UpdateProductDTO;
+use App\Tests\AbstractApplicationTestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UpdateProductDTOTest extends AbstractApplicationTestCase
+{
+    protected object $validator;
+    protected string $exampleValidName = 'Example name';
+    protected float $exampleValidPrice = 45.33;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->validator = static::getContainer()->get(ValidatorInterface::class);
+    }
+
+    public function testValidData(): void
+    {
+        $dto = new UpdateProductDTO(
+            name: $this->exampleValidName,
+            price: $this->exampleValidPrice
+        );
+
+        $errors = $this->validator->validate($dto);
+
+        $this->assertEmpty($errors);
+    }
+
+    /**
+     * @dataProvider invalidNameProvider
+     */
+    public function testInvalidName(string $name): void
+    {
+        $dto = new UpdateProductDTO(
+            name: $name,
+            price: $this->exampleValidPrice
+        );
+
+        $errors = $this->validator->validate($dto);
+
+        $this->assertCount(1, $errors);
+    }
+
+    public function invalidNameProvider(): iterable
+    {
+        yield [''];
+        yield ['1'];
+        yield [str_repeat('d', 101)];
+    }
+
+    /**
+     * @dataProvider invalidPriceProvider
+     */
+    public function testInvalidPrice(float $price): void
+    {
+        $dto = new UpdateProductDTO(
+            name: $this->exampleValidName,
+            price: $price
+        );
+
+        $errors = $this->validator->validate($dto);
+
+        $this->assertCount(1, $errors);
+    }
+
+    public function invalidPriceProvider(): iterable
+    {
+        yield [0.0];
+        yield [-1.1];
+    }
+}
