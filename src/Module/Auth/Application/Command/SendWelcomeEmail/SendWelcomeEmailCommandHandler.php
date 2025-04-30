03754fdd0e848704a1e08553245a27d7b97ea4f5
maciekiwaniuk <kazooooo@wp.pl>
2025-04-30 20:58:54 +0200

    Add sending welcome mail and add mailhog container

--- src/Module/Auth/Application/Command/SendWelcomeEmail/SendWelcomeEmailCommandHandler.php
+++ src/Module/Auth/Application/Command/SendWelcomeEmail/SendWelcomeEmailCommandHandler.php
@@ -5,18 +5,32 @@ declare(strict_types=1);
 namespace App\Module\Auth\Application\Command\SendWelcomeEmail;
 
 use App\Common\Application\AsyncCommand\AsyncCommandHandlerInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
 
 #[AsMessageHandler]
-class SendWelcomeEmailCommandHandler implements AsyncCommandHandlerInterface
+readonly class SendWelcomeEmailCommandHandler implements AsyncCommandHandlerInterface
 {
     public function __construct(
-
+        private MailerInterface $mailer,
+        private Environment $twig,
     ) {
     }
 
     public function __invoke(SendWelcomeEmailCommand $command): void
     {
-        // sending welcome email
+        $htmlBody = $this->twig->render('emails/welcome.html.twig', [
+            'fullName' => $command->dto->name . ' ' . $command->dto->surname,
+        ]);
+
+        $email = new Email()
+            ->from('no-reply@example.com')
+            ->to($command->dto->email)
+            ->subject('Welcome to our shop!')
+            ->html($htmlBody);
+
+        $this->mailer->send($email);
     }
 }
