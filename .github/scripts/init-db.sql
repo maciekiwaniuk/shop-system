632ddd74694af1f3ec884b9c2cd81fb2525a87b7
maciekiwaniuk <kazooooo@wp.pl>
2025-07-19 12:10:49 +0200

    Add env file for pipeline

--- .github/scripts/init-db.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `shop_system_auth`;
-CREATE DATABASE IF NOT EXISTS `shop_system_auth_test`;
-CREATE DATABASE IF NOT EXISTS `shop_system_commerce`;
-CREATE DATABASE IF NOT EXISTS `shop_system_commerce_test`;
-
-GRANT ALL PRIVILEGES ON *.* TO 'shop_user'@'%';
