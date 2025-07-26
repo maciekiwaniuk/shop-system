f9e56b927bd481481338e85c9a6f2e7a08567872
maciekiwaniuk <kazooooo@wp.pl>
2025-07-26 16:18:08 +0200

    Remove payment module from monolith

--- src/Module/Payment/Application/Command/ProcessPayment/ProcessPaymentCommandHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Module\Payment\Application\Command\ProcessPayment;
-
-class ProcessPaymentCommandHandler
-{
-}
--- src/Module/Payment/Infrastructure/Doctrine/Repository/PaymentRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Module\Payment\Infrastructure\Doctrine\Repository;
-
-class PaymentRepository
-{
-}
