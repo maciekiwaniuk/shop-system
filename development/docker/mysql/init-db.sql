f78d9a40708969aa9280da022c2b0052a7d472cf
maciekiwaniuk <kazooooo@wp.pl>
2025-07-31 17:10:47 +0200

    Add creating database for payments service

--- development/docker/mysql/init-db.sql
+++ development/docker/mysql/init-db.sql
@@ -2,5 +2,7 @@ CREATE DATABASE IF NOT EXISTS `shop_system_auth`;
 CREATE DATABASE IF NOT EXISTS `shop_system_auth_test`;
 CREATE DATABASE IF NOT EXISTS `shop_system_commerce`;
 CREATE DATABASE IF NOT EXISTS `shop_system_commerce_test`;
+CREATE DATABASE IF NOT EXISTS `shop_system_payments`;
+CREATE DATABASE IF NOT EXISTS `shop_system_payments_test`;
 
 GRANT ALL PRIVILEGES ON *.* TO 'shop_user'@'%';
