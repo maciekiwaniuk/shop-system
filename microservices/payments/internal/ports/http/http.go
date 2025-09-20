350c376bb0f912138614f70e918a19fe5e8972a1
maciekiwaniuk <iwaniukmaciej1@gmail.com>
2025-09-20 20:12:59 +0200

    Add create payer endpoint

--- microservices/payments/internal/ports/http/http.go
+++ microservices/payments/internal/ports/http/http.go
@@ -3,12 +3,8 @@ package http
 import (
 	"net/http"
 	"payments/internal/app"
-	"payments/internal/app/command"
-	"payments/internal/domain"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 )
 
 type HttpServer struct {
@@ -19,11 +15,10 @@ func NewHttpServer(app app.Application) HttpServer {
 	return HttpServer{app}
 }
 
-type CreatePayerRequest struct {
-	Id      string `json:"id" binding:"required"`
-	Email   string `json:"email" binding:"required,email,min=3,max=100"`
-	Name    string `json:"name" binding:"required,min=2,max=100"`
-	Surname string `json:"surname" binding:"required,min=2,max=100"`
+type Response struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 func (h HttpServer) SetupRouter(port string) error {
@@ -31,50 +26,23 @@ func (h HttpServer) SetupRouter(port string) error {
 
 	v1 := r.Group("/v1")
 	v1.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"success": true,
-			"message": "Payments service is working",
+		c.JSON(http.StatusOK, Response{
+			Success: true,
+			Message: "Payments service is working",
 		})
 	})
 
-	p := v1.Group("/payers")
-	p.POST("/create", func(c *gin.Context) {
-		var req CreatePayerRequest
-		if err := c.ShouldBindJSON(&req); err != nil {
-			zap.L().Error("nie tak", zap.Error(err))
-			c.JSON(http.StatusBadRequest, gin.H{
-				"success": false,
-				"message": err,
-			})
-			return
-		}
-
-		cmd := command.CreatePayer{
-			Payer: domain.Payer{
-				Id:        req.Id,
-				Email:     req.Email,
-				Name:      req.Name,
-				Surname:   req.Surname,
-				CreatedAt: time.Now(),
-				UpdatedAt: time.Now(),
-			},
-		}
-
-		if err := h.app.Commands.CreatePayer.Handle(c.Request.Context(), cmd); err != nil {
-			zap.L().Error("Failed to create payer", zap.Error(err))
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"success": false,
-				"message": "Something went wrong while creating payer",
-			})
-			return
-		}
-
-		c.JSON(http.StatusCreated, gin.H{
-			"success": true,
-			"message": "Payer created successfully",
-		})
-	})
+	setupPayerRoutes(v1, h.app)
 
 	err := r.Run(port)
 	return err
+}
+
+func setupPayerRoutes(rg *gin.RouterGroup, app app.Application) {
+	h := NewPayerHandler(app)
+
+	p := rg.Group("/payers")
+	{
+		p.POST("/create", h.CreatePayer)
+	}
 }
--- microservices/payments/internal/ports/http/payer_handler.go
+++ microservices/payments/internal/ports/http/payer_handler.go
@@ -1,1 +1,64 @@
 package http
+
+import (
+	"net/http"
+	"payments/internal/app"
+	"payments/internal/app/command"
+	"payments/internal/domain"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type PayerHandler struct {
+	app app.Application
+}
+
+func NewPayerHandler(app app.Application) PayerHandler {
+	return PayerHandler{app: app}
+}
+
+type CreatePayerRequest struct {
+	Id      string `json:"id" binding:"required"`
+	Email   string `json:"email" binding:"required,email,min=3,max=100"`
+	Name    string `json:"name" binding:"required,min=2,max=100"`
+	Surname string `json:"surname" binding:"required,min=2,max=100"`
+}
+
+func (h PayerHandler) CreatePayer(c *gin.Context) {
+	var req CreatePayerRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		zap.L().Error("binding create payer request failed", zap.Error(err))
+		c.JSON(http.StatusBadRequest, Response{
+			Success: false,
+			Message: "Something went wrong while binding payer request",
+		})
+		return
+	}
+
+	cmd := command.CreatePayer{
+		Payer: domain.Payer{
+			Id:        req.Id,
+			Email:     req.Email,
+			Name:      req.Name,
+			Surname:   req.Surname,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+	}
+
+	if err := h.app.Commands.CreatePayer.Handle(c.Request.Context(), cmd); err != nil {
+		zap.L().Error("Failed to create payer", zap.Error(err))
+		c.JSON(http.StatusInternalServerError, Response{
+			Success: false,
+			Message: "Something went wrong while creating payer",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, Response{
+		Success: true,
+		Message: "Payer created successfully",
+	})
+}
