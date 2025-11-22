6f7f5d79ff0416d5c33245c40dace925786d233b
maciekiwaniuk <iwaniukmaciej1@gmail.com>
2025-11-22 13:10:42 +0100

    Fix setting completed at field after completion of order

--- src/Module/Commerce/Application/Command/ChangeOrderStatus/ChangeOrderStatusCommandHandler.php
+++ src/Module/Commerce/Application/Command/ChangeOrderStatus/ChangeOrderStatusCommandHandler.php
@@ -6,9 +6,7 @@ namespace App\Module\Commerce\Application\Command\ChangeOrderStatus;
 
 use App\Common\Application\BusResult\CommandResult;
 use App\Common\Application\SyncCommand\SyncCommandHandlerInterface;
-use App\Module\Commerce\Domain\Entity\OrderStatusUpdate;
 use App\Module\Commerce\Domain\Repository\OrderRepositoryInterface;
-use App\Module\Commerce\Domain\Repository\OrderStatusUpdateRepositoryInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -18,7 +16,6 @@ use Throwable;
 readonly class ChangeOrderStatusCommandHandler implements SyncCommandHandlerInterface
 {
     public function __construct(
-        private OrderStatusUpdateRepositoryInterface $orderStatusUpdateRepository,
         private OrderRepositoryInterface $orderRepository,
         private LoggerInterface $logger,
     ) {
@@ -27,15 +24,22 @@ readonly class ChangeOrderStatusCommandHandler implements SyncCommandHandlerInte
     public function __invoke(ChangeOrderStatusCommand $command): CommandResult
     {
         try {
-            $changeOrderStatus = new OrderStatusUpdate(
-                order: $this->orderRepository->getReference($command->uuid),
-                status: $command->dto->status,
-            );
-            $this->orderStatusUpdateRepository->save($changeOrderStatus, true);
+            $order = $this->orderRepository->findByUuid($command->uuid);
+            if ($order === null) {
+                $this->logger->error('Order not found when changing status', [
+                    'order_uuid' => $command->uuid,
+                ]);
+                return new CommandResult(success: false, statusCode: Response::HTTP_NOT_FOUND);
+            }
+
+            $order->updateStatus($command->dto->status);
+            $this->orderRepository->save($order, true);
+
             return new CommandResult(success: true, statusCode: Response::HTTP_OK);
         } catch (Throwable $exception) {
             $this->logger->error('Failed to change order status', [
                 'new_status' => $command->dto->status->value,
+                'order_uuid' => $command->uuid,
                 'error' => $exception->getMessage(),
                 'exception_class' => get_class($exception),
                 'trace' => $exception->getTraceAsString(),
